0d6d3a1c08701172a9ea35065c0c58882b462380
Tonight11 <godofspeedman@gmail.com>
2024-10-29 16:38:21 +0600

    fix: breadcrumb link

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -2,7 +2,7 @@
   <nav>
     <ul class="breadcrumbs flex">
       <li v-for="(crumb, index) in breadcrumbs" :key="index">
-        <NuxtLink class="mx-2 underline" :to="`/category/${crumb.link}`">{{ crumb.name }}</NuxtLink>
+        <NuxtLink class="mx-2 underline" :to="`/${crumb.link}`">{{ crumb.name }}</NuxtLink>
         <span v-if="index < breadcrumbs.length - 1"> -> </span>
       </li>
     </ul>
